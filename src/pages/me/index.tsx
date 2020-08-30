4c68aeffe4d5b222e923e685c85ae4423762b7fd
Pav Sidhu <pav@pavsidhu.com>
2020-08-31 00:09:32 +0100

    me/index.tsx: add mobile top bar

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,3 +1,28 @@
+import Head from "next/head"
+import { styled } from "linaria/react"
+
+import TopBar from "../../components/TopBar"
+import SettingsIcon from "../../assets/icons/settings.svg"
+
+const Container = styled.div``
+
 export default function Me(): JSX.Element {
-  return <h1>Me</h1>
+  return (
+    <>
+      <Head>
+        <title>Me</title>
+      </Head>
+
+      <Container>
+        <TopBar
+          title="Hey Michael,"
+          subtitle="How are you feeling today?"
+          icon={{
+            icon: <SettingsIcon />,
+            href: "/settings",
+          }}
+        />
+      </Container>
+    </>
+  )
 }
