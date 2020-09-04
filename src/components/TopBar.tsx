9e47be2efcbc37637985ca0780721f3f2ff34bbd
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-09-04 11:09:15 +0100

    Add Jest tests for TabBar and TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -69,8 +69,8 @@ export default function TopBar(props: Props): JSX.Element {
     <Container>
       <Content>
         {backButton && (
-          <BackIconLink onClick={() => Router.back()}>
-            <BackIcon />
+          <BackIconLink onClick={() => Router.back()} data-test="top-bar-back-icon">
+            <BackIcon/>
           </BackIconLink>
         )}
 
--- test/components/TabBar.test.tsx
+++ test/components/TabBar.test.tsx
@@ -1,13 +1,23 @@
 import { shallow } from "enzyme"
-import TabBar from "../../src/components/TabBar"
+import TabBar, { Tab } from "../../src/components/TabBar"
 import * as nextRouter from "next/router"
+import each from "jest-each"
 
 nextRouter.useRouter = jest.fn()
-nextRouter.useRouter.mockImplementation(() => ({ route: "/" }))
 
 describe("Tab Bar: ", () => {
-  it("matches snapshot", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  each(['/me','/insights']).it('matches tab bar snapshot in %s', path => {
+    nextRouter.useRouter.mockImplementation(() => ({ pathname: path }))
     const wrapper = shallow(<TabBar/>)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  each([true, false]).it('matches tab snapshot when active=%s', isActive => {
+    const wrapper = shallow(<Tab active={isActive}/>)
     expect(wrapper).toMatchSnapshot()
   })
 })
