81e55fb234580e078a21c390478587f344e5f6d2
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-09-12 12:40:16 +0100

    Use category assertions instead of preset

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,10 @@ module.exports = {
       target: "temporary-public-storage"
     },
     assert: {
-      preset: "lighthouse:recommended"
+      // preset: "lighthouse:recommended" TODO: enable and fix findings
+      "categories:performance": ["error", {"minScore": 0.8}],
+      "categories:accessibility": ["error", {"minScore": 1}],
+      "categories:seo": ["error", {"minScore": 1}]
     }
   }
 }
