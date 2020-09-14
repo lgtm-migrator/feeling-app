54572f8710dbdcd4a44c427f4643bc6e058c5bb9
Pav Sidhu <pav@pavsidhu.com>
2020-09-14 11:18:37 +0100

    emotions.ts: add emotions helper

---
+++ src/utils/emotions.ts
@@ -0,0 +1,47 @@
+import AngryIcon from "../assets/emojis/angry.svg"
+import UpsetIcon from "../assets/emojis/upset.svg"
+import UnsureIcon from "../assets/emojis/unsure.svg"
+import GreatIcon from "../assets/emojis/great.svg"
+import AmazingIcon from "../assets/emojis/amazing.svg"
+
+export enum Emotion {
+  ANGRY = "angry",
+  UPSET = "upset",
+  UNSURE = "unsure",
+  GREAT = "great",
+  AMAZING = "amazing",
+}
+
+export function getEmotionColor(emotion: Emotion): string {
+  switch (emotion) {
+    case Emotion.ANGRY:
+      return "var(--purple-color)"
+    case Emotion.UPSET:
+      return "var(--blue-color)"
+    case Emotion.UNSURE:
+      return "var(--green-color)"
+    case Emotion.GREAT:
+      return "var(--yellow-color)"
+    case Emotion.AMAZING:
+      return "var(--red-color)"
+    default:
+      return "var(--light-grey-color)"
+  }
+}
+
+export function getEmojiSvg(emotion: Emotion): React.FunctionComponent {
+  switch (emotion) {
+    case Emotion.ANGRY:
+      return AngryIcon
+    case Emotion.UPSET:
+      return UpsetIcon
+    case Emotion.UNSURE:
+      return UnsureIcon
+    case Emotion.GREAT:
+      return GreatIcon
+    case Emotion.AMAZING:
+      return AmazingIcon
+    default:
+      return undefined
+  }
+}
