74568b2b70c0f7c561bdd1e590490d8261b4acda
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-09-16 16:03:54 +0100

    Use lighthouse preset

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,15 +1,10 @@
 module.exports = {
   ci: {
     upload: {
-      target: "temporary-public-storage"
+      target: "temporary-public-storage",
     },
     assert: {
-      // preset: "lighthouse:recommended" TODO: enable and fix findings
-      assertions: {
-        "categories:performance": ["error", { "minScore": 0.8 }],
-        "categories:accessibility": ["error", { "minScore": 0.8 }],
-        "categories:seo": ["error", { "minScore": 0.8 }]
-      }
-    }
-  }
+      preset: "lighthouse:recommended",
+    },
+  },
 }
