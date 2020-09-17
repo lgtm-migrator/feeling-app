4164b735e6c4f161b680803a4c9432ec0df95bcd
Pav Sidhu <pav@pavsidhu.com>
2020-09-17 14:31:19 +0100

    dates.ts: adjust min and max dates

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,9 +1,10 @@
 import addMonths from "date-fns/addMonths"
-import { differenceInMonths } from "date-fns/esm"
+import addYears from "date-fns/addYears"
+import differenceInMonths from "date-fns/differenceInMonths"
 
 // Specify limits for the calendar in the Me tab
-export const MIN_DATE = new Date(2000, 0, 1)
-export const MAX_DATE = new Date(2001, 0, 1)
+export const MIN_DATE = new Date(2020, 0, 1)
+export const MAX_DATE = addYears(new Date(), 1)
 
 export const getDateFromIndex = (index: number): Date =>
   addMonths(MIN_DATE, index)
