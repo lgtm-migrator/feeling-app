d4c76bf0bbd4a52539a1e86719a7e2f409c30b78
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-09-17 16:49:12 +0100

    Fix returned tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head"
 
 export default function Home(): JSX.Element {
-  return
-  ;<>
-    <Head>
-      <title>Hello, World!</title>
-      <meta name="description" content="Hello, World!"></meta>
-    </Head>
-    <h1>Hello, World!</h1>
-  </>
+  return (
+    <>
+      <Head>
+        <title>Hello, World!</title>
+        <meta name="description" content="Hello, World!"></meta>
+      </Head>
+      <h1>Hello, World!</h1>
+    </>
+  )
 }
