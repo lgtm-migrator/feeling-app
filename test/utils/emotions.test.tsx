6cb97fcae7f50acc3c1a6b70d7ebdc616d7c1774
Pav Sidhu <pav@pavsidhu.com>
2020-09-26 13:21:09 +0100

    fix emotions test

--- test/utils/emotions.test.tsx
+++ test/utils/emotions.test.tsx
@@ -1,27 +1,43 @@
+import each from "jest-each"
 import { Emotion, getEmotionColor, renderEmoji } from "../../src/utils/emotions"
 
+import AngryIcon from "../assets/emojis/angry.svg"
+import UpsetIcon from "../assets/emojis/upset.svg"
+import UnsureIcon from "../assets/emojis/unsure.svg"
+import GreatIcon from "../assets/emojis/great.svg"
 import AmazingIcon from "../assets/emojis/amazing.svg"
 
 describe("Render an emotion's emoji: ", () => {
-  it("returns an emoji", () => {
-    const result = renderEmoji(Emotion.AMAZING)
-    expect(result).toEqual(<AmazingIcon />)
-  })
+  const emotionIcons = [
+    { emotion: Emotion.ANGRY, icon: <AngryIcon /> },
+    { emotion: Emotion.UPSET, icon: <UpsetIcon /> },
+    { emotion: Emotion.UNSURE, icon: <UnsureIcon /> },
+    { emotion: Emotion.GREAT, icon: <GreatIcon /> },
+    { emotion: Emotion.AMAZING, icon: <AmazingIcon /> },
+    [undefined, undefined],
+  ]
 
-  it("returns nothing", () => {
-    const result = renderEmoji()
-    expect(result).toBeUndefined()
+  each(emotionIcons).it("returns an emoji", (emotionIcon) => {
+    const result = renderEmoji(emotionIcon.emotion)
+    expect(result).toEqual(emotionIcon.icon)
   })
 })
 
 describe("Emotion color: ", () => {
-  it("returns the color used to represent the emotion", () => {
-    const result = getEmotionColor(Emotion.ANGRY)
-    expect(result).toEqual("var(--purple-color)")
-  })
+  const emotionColors = [
+    { emotion: Emotion.ANGRY, color: "var(--purple-color)" },
+    { emotion: Emotion.UPSET, color: "var(--blue-color)" },
+    { emotion: Emotion.UNSURE, color: "var(--green-color)" },
+    { emotion: Emotion.GREAT, color: "var(--yellow-color)" },
+    { emotion: Emotion.AMAZING, color: "var(--red-color)" },
+    [undefined, "var(--light-grey-color)"],
+  ]
 
-  it("returns a placeholder color", () => {
-    const result = getEmotionColor()
-    expect(result).toEqual("var(--light-grey-color)")
-  })
+  each(emotionColors).it(
+    "returns the color used to represent the emotion",
+    (emotionColor) => {
+      const result = getEmotionColor(emotionColor.emotion)
+      expect(result).toEqual(emotionColor.color)
+    }
+  )
 })
