863f1005fbffeb409c8e8e23e4625bab44d77515
Pav Sidhu <pav@pavsidhu.com>
2020-09-26 12:32:05 +0100

    Actually fix date tests

--- test/utils/dates.test.ts
+++ test/utils/dates.test.ts
@@ -3,12 +3,12 @@ import { getDateFromIndex, getIndexFromDate } from "../../src/utils/dates"
 describe("Dates: ", () => {
   it("Gets the date by index", () => {
     const date = getDateFromIndex(20)
-    const expectedDate = new Date(2021, 9, 1).toISOString()
+    const expectedDate = new Date(2021, 8, 1).toISOString()
     expect(date.toISOString()).toEqual(expectedDate)
   })
 
   it("Gets the index from date", () => {
-    const index = getIndexFromDate(new Date(2001, 9, 1))
+    const index = getIndexFromDate(new Date(2021, 8, 1))
     expect(index).toEqual(20)
   })
 })
