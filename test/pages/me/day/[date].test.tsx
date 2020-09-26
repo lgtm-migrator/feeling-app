ff98f23d99d952cc4a641cd88970db39b82a8397
Pav Sidhu <pav@pavsidhu.com>
2020-09-26 11:58:27 +0100

    day page tests mock router

--- test/pages/me/day/[date].test.tsx
+++ test/pages/me/day/[date].test.tsx
@@ -1,8 +1,13 @@
 import { shallow } from "enzyme"
 import Date from "../../../../src/pages/me/day/[date]"
 
+nextRouter.useRouter = jest.fn()
+
 describe("Me: ", () => {
   it("matches snapshot", () => {
+    nextRouter.useRouter.mockImplementation(() => ({
+      query: { date: "01-01-2020" },
+    }))
     const wrapper = shallow(<Date />)
     expect(wrapper).toMatchSnapshot()
   })
