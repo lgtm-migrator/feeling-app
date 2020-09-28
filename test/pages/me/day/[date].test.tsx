522f45b26456f05f908d7622c147cdb4947c4607
Pav Sidhu <pav@pavsidhu.com>
2020-09-28 21:46:17 +0100

    [date].test.tsx: add query tests

--- test/pages/me/day/[date].test.tsx
+++ test/pages/me/day/[date].test.tsx
@@ -36,3 +36,21 @@ describe("Me: ", () => {
     expect(mockPush).toHaveBeenCalledWith("/me")
   })
 })
+
+it("Renders nothing when date is not provided", () => {
+  nextRouter.useRouter.mockImplementation(() => ({
+    query: { date: null },
+  }))
+  const wrapper = shallow(<Date />)
+  expect(wrapper).toMatchSnapshot()
+})
+
+it("Redirects to /me when no date is provided", () => {
+  const mockPush = jest.fn()
+  nextRouter.useRouter.mockImplementation(() => ({
+    query: { date: null },
+    push: mockPush,
+  }))
+  const wrapper = mount(<Date />)
+  expect(mockPush).toHaveBeenCalledWith("/me")
+})
